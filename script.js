0864955cd7784afba9880b6a2ff19f301ebab351
Despresso <118217896+Despresso16@users.noreply.github.com>
2024-12-11 13:37:51 +0100

    last update
    even more info

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ const temp = document.querySelector("#temp")
 const desc = document.querySelector("#weather-desc")
 const icon = document.querySelector("#weather-icon")
 const clouds = document.querySelector("#clouds")
+const humidity = document.querySelector("#humi")
+const pressure = document.querySelector("#pressure")
+const wind = document.querySelector("#wind")
+const windDir = document.querySelector("#windDir")
 const rain = document.querySelector("#rain")
 const snow = document.querySelector("#snow")
 const error = document.querySelector("#error")
@@ -17,7 +21,7 @@ const viewCustom = () =>{
         customTimeCont.style.opacity = 1;
     } 
     else{
-        customTimeCont.style.height = "0vh";
+        customTimeCont.style.height = 0;
         customTimeCont.style.opacity = 0;
     }
 }
@@ -56,7 +60,7 @@ const viewWeather = () =>{
     }
     
     if(timeInput.value == "Teraz"){
-        fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat.toFixed(2)}&longitude=${long.toFixed(2)}&current=temperature_2m,rain,snowfall,cloud_cover&hourly=temperature_2m,rain,snowfall,cloud_cover&timezone=Europe%2FBerlin`)
+        fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat.toFixed(2)}&longitude=${long.toFixed(2)}&current=temperature_2m,relative_humidity_2m,rain,snowfall,cloud_cover,surface_pressure,wind_speed_10m,wind_direction_10m&hourly=temperature_2m,relative_humidity_2m,rain,snowfall,surface_pressure,cloud_cover,wind_speed_10m,wind_direction_10m`)
         .then(res => res.json())
         .then(data => writeViewNow(data));
     }
@@ -71,23 +75,52 @@ const viewWeather = () =>{
             error.style.visibility = "visible";
             return;
         }
-        fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat.toFixed(2)}&longitude=${long.toFixed(2)}&current=temperature_2m,rain,snowfall,cloud_cover&hourly=temperature_2m,rain,snowfall,cloud_cover&timezone=Europe%2FBerlin`)
+        fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat.toFixed(2)}&longitude=${long.toFixed(2)}&current=temperature_2m,relative_humidity_2m,rain,snowfall,cloud_cover,surface_pressure,wind_speed_10m,wind_direction_10m&hourly=temperature_2m,relative_humidity_2m,rain,snowfall,surface_pressure,cloud_cover,wind_speed_10m,wind_direction_10m`)
         .then(res => res.json())
         .then(data => writeViewLater(data, parseInt(customTime.value) -1));
     }
     else {
-        fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat.toFixed(2)}&longitude=${long.toFixed(2)}&current=temperature_2m,rain,snowfall,cloud_cover&hourly=temperature_2m,rain,snowfall,cloud_cover&timezone=Europe%2FBerlin`)
+        fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat.toFixed(2)}&longitude=${long.toFixed(2)}&current=temperature_2m,relative_humidity_2m,rain,snowfall,cloud_cover,surface_pressure,wind_speed_10m,wind_direction_10m&hourly=temperature_2m,relative_humidity_2m,rain,snowfall,surface_pressure,cloud_cover,wind_speed_10m,wind_direction_10m`)
         .then(res => res.json())
         .then(data => writeViewLater(data));
     }
 }
 const writeViewNow = (data) =>{
-    console.log(data);
     cityDisplay.textContent = `${cityInput.value} - Teraz`
     temp.innerHTML = `<i class="wi wi-thermometer"></i>  Temperatura: ${data.current.temperature_2m}°C`;
-    clouds.innerHTML = `<i class="wi wi-cloudy"></i>  Zachmurzenie: ${data.current.cloud_cover.toFixed(2)}%`
+    clouds.innerHTML = `<i class="wi wi-cloudy"></i>  Zachmurzenie: ${data.current.cloud_cover}%`
     rain.innerHTML = `<i class="wi wi-sprinkle"></i>  Ilość deszczu ${data.current.rain}mm`
     snow.innerHTML = `<i class="wi wi-snow"></i>  Ilość śniegu: ${data.current.snowfall}cm`
+    humidity.innerHTML = `<i class="wi wi-raindrops"></i>  Wilgotność: ${data.current.relative_humidity_2m}%`
+    wind.innerHTML = `<i class="wi wi-strong-wind"></i>  Prędkośc wiatru: ${data.current.wind_speed_10m} km/h`
+    pressure.innerHTML = `<i class="wi wi-barometer"></i> Ciśnienie: ${data.current.surface_pressure} hPa`
+    let direction = "";
+    let dirValue = data.current.wind_direction_10m;
+    if(dirValue >= 350 || dirValue <= 10){
+        direction = "Północ"
+    }
+    else if(dirValue > 10 && dirValue < 80){
+        direction = "Północny-Wschód"
+    }
+    else if(dirValue >= 80 && dirValue <= 100){
+        direction = "Wschód"
+    }
+    else if(dirValue > 100 && dirValue < 170){
+        direction = "Południowy-Wschód"
+    }
+    else if(dirValue >= 170 && dirValue <= 190 ){
+        direction = "Południe"
+    }
+    else if(dirValue > 190 && dirValue < 260){
+        direction = "Południowy-Zachód"
+    }
+    else if(dirValue >= 260 && dirValue <= 280){
+        direction = "Zachód"
+    }
+    else {
+        direction = "Północny-Zachód"
+    }
+    windDir.innerHTML = `<i class="wi wi-wind-direction"></i>  Kierunek wiatru: ${direction}`
     icon.classList.remove("wi-cloud");
     icon.classList.remove("wi-day-sleet")
     icon.classList.remove("wi-day-rain")
@@ -124,9 +157,39 @@ const writeViewNow = (data) =>{
 const writeViewLater = (data, h = timeInput.value - 1) =>{
     cityDisplay.textContent = `${cityInput.value} - Za ${h + 1}h`
     temp.innerHTML = `<i class="wi wi-thermometer"></i>  Temperatura: ${data.hourly.temperature_2m[h]}°C`;
-    clouds.innerHTML = `<i class="wi wi-cloudy"></i>  Zachmurzenie: ${data.hourly.cloud_cover[h].toFixed(2)}%`
+    clouds.innerHTML = `<i class="wi wi-cloudy"></i>  Zachmurzenie: ${data.hourly.cloud_cover[h]}%`
     rain.innerHTML = `<i class="wi wi-sprinkle"></i>  Ilość deszczu ${data.hourly.rain[h]}mm`
     snow.innerHTML = `<i class="wi wi-snow"></i>  Ilość śniegu: ${data.hourly.snowfall[h]}cm`
+    humidity.innerHTML = `<i class="wi wi-raindrops"></i>  Wilgotność: ${data.hourly.relative_humidity_2m[h]}%`
+    wind.innerHTML = `<i class="wi wi-strong-wind"></i>  Prędkośc wiatru: ${data.hourly.wind_speed_10m[h]} km/h`
+    pressure.innerHTML = `<i class="wi wi-barometer"></i> Ciśnienie: ${data.hourly.surface_pressure[h]} hPa`
+    let direction = "";
+    let dirValue = data.hourly.wind_direction_10m[h];
+    if(dirValue >= 350 || dirValue <= 10){
+        direction = "Północ"
+    }
+    else if(dirValue > 10 && dirValue < 80){
+        direction = "Północny-Wschód"
+    }
+    else if(dirValue >= 80 && dirValue <= 100){
+        direction = "Wschód"
+    }
+    else if(dirValue > 100 && dirValue < 170){
+        direction = "Południowy-Wschód"
+    }
+    else if(dirValue >= 170 && dirValue <= 190 ){
+        direction = "Południe"
+    }
+    else if(dirValue > 190 && dirValue < 260){
+        direction = "Południowy-Zachód"
+    }
+    else if(dirValue >= 260 && dirValue <= 280){
+        direction = "Zachód"
+    }
+    else {
+        direction = "Północny-Zachód"
+    }
+    windDir.innerHTML = `<i class="wi wi-wind-direction"></i>  Kierunek wiatru: ${direction}`
     icon.classList.remove("wi-cloud");
     icon.classList.remove("wi-day-sleet")
     icon.classList.remove("wi-day-rain")
